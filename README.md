49168297ef7de88b7960d020f7c2128399cc64d8
Alexander Meißner <AlexanderMeissner@gmx.net>
2020-03-05 12:01:41 +0100

    Adds basic setup

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # LinuxSetup
-Configuration: Arch Linux + Sway (Wayland) on a MacBook Pro
+This is my personal Arch Linux setup.
+It uses the [2020.03.01 release](magnet:?xt=urn:btih:54da0b79719064aa10fe2cc4e13630a1222d1939&dn=archlinux-2020.03.01-x86_64.iso&tr=udp://tracker.archlinux.org:6969&tr=http://tracker.archlinux.org:6969/announce) with a 5.5.6 linux kernel.
+Donwload and copy it onto an external drive (e.g. SB-stick or SD-card) and then follow the instructions in the [setup](setup.sh).
---
+++ setup.sh
@@ -0,0 +1,71 @@
+loadkeys de-latin1
+timedatectl set-ntp true
+fdisk -l
+
+cgdisk /dev/sdb
+mkswap /dev/sdb3
+swapon /dev/sdb3
+mkfs.btrfs /dev/sdb4
+mount /dev/sdb4 /mnt
+btrfs subvol create /mnt/@root
+umount /mnt
+mount /dev/sdb4 /mnt -o subvol=@root
+mkdir -p /mnt/mnt/bootloader /mnt/mnt/mac
+mount /dev/sdb2 /mnt/mnt/bootloader
+mount /dev/sdb5 /mnt/mnt/mac
+nano /etc/packman.d/mirrorlist
+
+pacstrap -D /mnt base linux-headers sudo fakeroot htop git patch vim make cmake meson ninja pkg-config lld lldb llvm clang grub grub-btrfs btrfs-progs hfsprogs dhcpcd iw openssh xf86-video-intel xf86-video-nouveau broadcom-wl sway light alacritty alsa-utils firefox
+genfstab -U /mnt >> /mnt/etc/fstab
+arch-chroot /mnt
+
+LANG='en_US.UTF-8'
+HOSTNAME=
+USERNAME=
+PASSWORD=
+
+rm /etc/localtime
+ln -s /usr/share/zoneinfo/Europe/Berlin /etc/localtime
+hwclock --systohc
+sed -i "s/#$LANG/$LANG/g" /etc/locale.gen
+locale-gen
+echo "LANG=$LANG" > /etc/locale.conf
+echo "KEYMAP=de-latin1" > /etc/vconsole.conf
+echo "$HOSTNAME" > /etc/hostname
+echo "export MOZ_ENABLE_WAYLAND=1" > /etc/profile.d/firefox.sh
+useradd -m -g users -G wheel -s /bin/bash $USERNAME
+echo -e "$PASSWORD\n$PASSWORD" | passwd $USERNAME
+echo -e "$PASSWORD\n$PASSWORD" | passwd
+mv ./sudoers /etc/sudoers
+# visudo
+systemctl enable dhcpcd.service
+
+su $USERNAME
+cd ~
+mkdir -p ~/.config/sway/
+mv ./sway.config ~/.config/sway/config
+git clone https://aur.archlinux.org/hfsprogs.git
+cd hfsprogs
+makepkg -si
+cd ..
+git clone https://aur.archlinux.org/wob.git
+cd wob
+meson build
+ninja -C build
+sudo ninja -C build install
+cd ..
+git clone https://aur.archlinux.org/wofi.git
+cd wofi
+meson build
+ninja -C build
+sudo ninja -C build install
+cd ..
+exit
+
+mkdir -p /boot/grub /mnt/bootloader/mach_kernel /mnt/bootloader/System/Library/CoreServices
+grub-mkconfig -o /boot/grub/grub.cfg
+grub-mkstandalone -o /mnt/bootloader/System/Library/CoreServices/boot.efi -d /usr/lib/grub/x86_64-efi -O x86_64-efi --compress=xz /boot/grub/grub.cfg
+sed -i 's/BINARIES=()/BINARIES=("\/usr\/bin\/btrfs")/g' /etc/mkinitcpio.conf
+mkinitcpio -P
+exit
+reboot
